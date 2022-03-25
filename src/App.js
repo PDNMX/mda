74fac4ce1dfa1833f80625ceae4bea793fb9022c
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2022-03-25 14:16:57 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
         <ThemeProvider theme={theme}>
             <Router>
                 <Switch>
-                    <Route exact path="/">
+                    <Route basename={process.env.PUBLIC_URL}>
                         <Home/>
                     </Route>
                     {/*<Route exact path="/product/:id">
