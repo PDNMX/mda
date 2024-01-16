d0c705210634885154fbcf9fe7783fdd1608372a
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2024-01-16 16:15:33 -0600

    fix build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,22 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: '../build',
-  },
-  plugins: [react()],
-  esbuild: {
-    loader: {
-      '.js': 'jsx',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
     },
   },
+  plugins: [react()],
 });
